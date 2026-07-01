6e2cccaade97b78ac6820bfb7735a6c794ef3311
HaMinhChau <haminhchau1106@gmail.com>
2026-07-01 19:48:03 +0700

    lap2

--- fe1-web2081-summer26-main/src/app/app.routes.ts
+++ fe1-web2081-summer26-main/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Home } from './home/home';
+import { Products } from './products/products';
+export const routes: Routes = [
+    { path: 'about', component: About },
+    { path: 'contact', component: Contact },
+    { path: 'home', component: Home },
+    { path: 'products', component: Products },
+];
 
-export const routes: Routes = [];
