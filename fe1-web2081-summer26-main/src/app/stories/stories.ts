efbaca49c3ef6cb767806845b46e7502d995365c
HaMinhChau <haminhchau1106@gmail.com>
2026-07-15 19:31:49 +0700

    lap5

--- fe1-web2081-summer26-main/src/app/stories/stories.ts
+++ fe1-web2081-summer26-main/src/app/stories/stories.ts
@@ -1,39 +1,61 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+interface Story {
+  id: number;
+  title: string;
+  author: string;
+  price: number;
+  imageUrl: string;
+  views: number;
+  since: string;
+}
 
 @Component({
   selector: 'app-stories',
-  standalone: true, 
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './stories.html',
   styleUrl: './stories.css',
 })
 export class Stories {
-  stories = [
-    {
-      title: "Dragon Ball",
-      since: "1980",
-      author: "Tin",
-      price: "50000",
-      views: 100000,
-      imageUrl: "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/03/Attack-On-Titan-Characters.jpg"
-    },
-    {
-      title: "Attack On Titan",
-      since: "2010",
-      author: "Bảo",
-      price: "50000",
-      views: 900000,
-      imageUrl: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRBJQEw7i9GWRWgW9YTyJqot8WLJfFMoPBsmg3dIVNoVXMAnqDtqkBsZG9gVgOc"
-    },
-    {
-      title: "Bleach",
-      since: "1990",
-      author: "Băng",
-      price: "5000",
-      views: 400000,
-      imageUrl: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRbprTrqQv2sq_mSBcxrX8B4TsM682Vrd4UGKJYWqBbmcohhPGrSvAnWlV8cVFy"
-    }
-  ];
+  stories: Story[] = [];
+
+  loading = false;
+  error = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getStories();
+  }
+
+  getStories() {
+    this.http.get<Story[]>("http://localhost:3000/stories").subscribe({
+      next: (data) => {
+        this.stories = data;
+        this.loading = false;
+      },
+      error: () => {
+        console.log("Không thể tải dữ liệu");
+        this.loading = false;
+        this.error = true;
+      },
+    });
+  }
+
+  deleteStory(id: number) {
+    if (!confirm("Có muốn xoá không")) return;
+
+    this.http.delete(`http://localhost:3000/stories/${id}`).subscribe({
+      next: () => {
+        this.stories = this.stories.filter(story => story.id !== id);
+        alert("Xoá thành công");
+      },
+      error: () => {
+        alert("Xoá thất bại");
+      },
+    });
+  }
 }
